2964c8c610748f5ab95e7259f8526f2df3b6ee31
pedrosilvamnchunks <pedrosilvamnchunks@gmail.com>
2024-06-05 17:19:21 +0000

    Network up and cadvisor in kubernetes also set

--- Benchmarking/kubernetes-network/clear.sh
+++ Benchmarking/kubernetes-network/clear.sh
@@ -1,9 +1,24 @@
-# Delete existing resources
+# DELETE EVERYTHING / STOP EVERYTHING
 for file in ./kubernetes-components/*/*.yaml; do
     kubectl delete -f "$file" --force
 done
-
-# Apply the updated resources
-for file in ./kubernetes-components/*/*.yaml; do
+# INIT THE COUCHS FIRSTLY
+for file in ./kubernetes-components/org*-couch-*/*.yaml; do
     kubectl apply -f "$file"
 done
+# INIT THE PEERS
+for file in ./kubernetes-components/org*-p*/*.yaml; do
+    kubectl apply -f "$file"
+done
+# INIT ORDERERS
+for file in ./kubernetes-components/org*-or*/*.yaml;do
+    kubectl apply -f "$file"
+done
+# INIT THE REST
+kubectl apply -f ./ca/.
+kubectl apply -f ./tls-ca/.
+kubectl apply -f ./cadvisor/.
+kubectl apply -f ./multi-client/.
+kubectl apply -f ./basic-chaincode/.
+kubectl apply -f ./load-balancer/.
+kubectl apply -f ./load-balancer-all-peers-service/.
---
+++ Benchmarking/kubernetes-network/kuber-components/cadvisor/readme.md
@@ -0,0 +1,3 @@
+# info
+- label the nodes with their name like "kubectl label nodes chp-srvaida70 node-name=chp-srvaida70"
+- Putting a cadvisor in every node
